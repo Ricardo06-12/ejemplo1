09c3a871594875708d76d32a043973c70f1c87b2
Ricardo06-12 <ricardotmilian@gmail.com>
2025-04-14 13:47:56 -0500

    buenos dias
    se cambio el hola por el buenos dias

--- src/ejercicio1/Ejercicio1.java
+++ src/ejercicio1/Ejercicio1.java
@@ -6,7 +6,7 @@ public class Ejercicio1 {
 
     
     public static void main(String[] args) {
-        System.out.println("hola"); // hey
+        System.out.println("buenos dias"); // se muestra buenos dias
     }
     
 }
