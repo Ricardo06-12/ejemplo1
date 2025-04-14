4c5aba5930b07291e4d76bd2008ead67be98aeb8
Ricardo06-12 <ricardotmilian@gmail.com>
2025-04-14 13:51:48 -0500

    se cambio a buenas noches
    se muestra  buenas noches y un comentario que dice que se muestra el mensaje de buenas noches

--- src/ejercicio1/Ejercicio1.java
+++ src/ejercicio1/Ejercicio1.java
@@ -6,7 +6,7 @@ public class Ejercicio1 {
 
     
     public static void main(String[] args) {
-        System.out.println("buenos dias"); // se muestra buenos dias
+        System.out.println("buenas noches"); // se muestra el mensaje buenas noches 
     }
     
 }
